d7c0d056a777b59dfcbc217a5ec487c2959651b3
Guohao Dou <gdou2@illinois.edu>
2017-08-19 21:21:46 -0700

    algo designed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,33 @@ import 'rxjs/add/operator/pairwise';
 import { Class, Section, Meeting, Range } from './class-section'
 import { SchedulingToolkitService } from './scheduling-toolkit.service'
 
+/**
+ * IMPORTANT: To avoid further confusion, we call sections 
+ * like the A, B section of CS 173 "big sections"
+ * 
+ * And the sections of a class, like its lab, lecture, discussion, "sections"
+ */
+
+/**
+ * Algorithm to do exhaustive scheduling
+ * 
+ * e.g. We have CS 173 and MATH 241 to put into our schedule.
+ * CS 173 has 2 big sections
+ * MATH 241 has 5 big sections
+ * 
+ *  // put this chunk into a generator
+ *  for bigsec173 in CS173.bigsections:
+ *      for bigsec241 in CS241.bigsections:
+ *          for lec173 in bigsec173.lectures:
+ *              for dis173 in bigsec173.discussions:
+ *                  for lec241 in bigsec241.lectures:
+ *                      for dis241 in bigsec241.discussions:
+ *                          tryPutIntoSchedule(lec173, lec241, dis173, dis241) // yield something
+ * 
+ *  PROBLEM: writing nested for loop of a variable amount 
+ *           of iterables is possible but notoriously hard
+ */
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -42,6 +69,8 @@ export class AppComponent implements OnInit {
         // Necessary because of the way pairwise works
         this.courseNamesObservable = this.courseNamesSubject.asObservable();
 
+        this.tryout();
+
         this.courseNamesObservable.pairwise()
                 .subscribe(twoCourses => {
                     if (this.isChanged(twoCourses[0], twoCourses[1])) {
@@ -50,6 +79,14 @@ export class AppComponent implements OnInit {
                         this.goOnWithCurrFSM();
                     }
                 })
+    }
+
+    tryout() {
+        this.cis.getCoursesInfoByNameMock(['CS 173']).then(c => {
+            const cls = c[0];
+            console.log(this.stk.groupClassSectionBySectionAB(cls));
+            console.log(this.stk.groupClassSectionBySectionType(this.stk.groupClassSectionBySectionAB(cls)[0]));
+        })
     }
 
     resetStateMachine() {
--- src/app/scheduling-toolkit.service.ts
+++ src/app/scheduling-toolkit.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Class, Section, Meeting, Range } from './class-section'
+import * as _ from "lodash";
 
 @Injectable()
 export class SchedulingToolkitService {
 
     constructor() { }
 
+    /**
+     * return true if two ranges overlap.
+     * @param r1 
+     * @param r2 
+     */
     rangeOverlap(r1: Range, r2: Range): boolean {
         return r1.from <= r2.to && r1.to >= r2.from;
     }
 
-    meetingsOverlap(m1: Meeting, m2: Meeting) {
+    /**
+     * the meeting of a section consist of many ranges
+     * e.g. MW 2:00-3:00 => ['M 2:00-3:00', 'W 2:00-3:00']
+     * check all the ranges in two Meetings to detect overlap
+     * @param m1 
+     * @param m2 
+     */
+    meetingOverlap(m1: Meeting, m2: Meeting) {
         const ranges_1 = m1.ranges;
         const ranges_2 = m2.ranges;
         for (const r1 of ranges_1) {
@@ -23,10 +36,44 @@ export class SchedulingToolkitService {
         return false;
     }
 
+    /**
+     * a trivial wrapper of meetingOverlap
+     * @param s1 
+     * @param s2 
+     */
     sectionOverlap(s1: Section, s2: Section) {
         const m1 = s1.meetings;
         const m2 = s2.meetings;
-        return this.meetingsOverlap(m1, m2);
+        return this.meetingOverlap(m1, m2);
+    }
+
+    /**
+     * Methods to preproc the course data
+     * (1) group by "big section"(A section, B section)
+     * (2) group by type(lecture, discussion)
+     */
+
+    /**
+     * e.g. CS 173 has A section and B section.
+     * both A and B section has the set of classes needed(a lecture and a discussion)
+     * 
+     * this is a method to group sections of a class by "big sections"
+     * (dude I know the terminology sucks)
+     * 
+     * @param course 
+     */
+    groupClassSectionBySectionAB(course: Class): Section[][] {
+        const sections =  _.groupBy(course.sections, sec => sec.section[0]);
+        return _.values(sections);
+    }
+
+    /**
+     * group the sections based on their "type". i.e. "LEC", "DIS", "LCD", "LBD"......
+     * @param sections 
+     */
+    groupClassSectionBySectionType(sections: Section[]): Section[][] {
+        const sectionGroups = _.groupBy(sections, sec => sec.type);
+        return _.values(sectionGroups);
     }
 
 }
